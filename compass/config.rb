9b3c2c171310582d7b3f75027eb4ceb14cbd616c
lars.eggert@nokia.com <lars.eggert@nokia.com>
2011-04-26 09:20:55 +0000

    Compress output.

--- compass/config.rb
+++ compass/config.rb
@@ -4,7 +4,7 @@ css_dir      = "output/"
 sass_dir     = "content/" 
 images_dir   = "/"
 
-# when using SCSS:
 sass_options = {
-  :syntax => :scss
+  :style             => :compressed,
+  :syntax            => :scss
 }
