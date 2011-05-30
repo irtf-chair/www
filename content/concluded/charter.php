46b7b7da72ededb5c8a63af579d64850fe847181
lars.eggert@nokia.com <lars.eggert@nokia.com>
2011-05-30 08:42:29 +0000

    Redirects for more broken URLs.

--- content/concluded/charter.php
+++ content/concluded/charter.php
@@ -1,6 +1,12 @@
 <?php
 if (preg_match("/^((as|cf|dtn|hip|icc|nm|p2p|r|sam|tm|vn)rg|mobopts)$/i", $_GET["group"])) {
 	$tgt = strtolower($_GET["group"]);
+} elseif ($_GET["group"] == "hip") {	
+        $tgt = "hiprg";
+} elseif ($_GET["group"] == "p2pr") {	
+        $tgt = "p2prg";
+} elseif ($_GET["group"] == "cfr") {	
+        $tgt = "cfrg";
 } else {
 	$tgt = "concluded/" . strtolower($_GET["group"]);
 }
