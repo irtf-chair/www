b86644a1550e66fdbea0052c209e338966e95176
lars@netapp.com <lars@netapp.com>
2013-05-09 08:38:28 +0000

    Replace Jari with Russ.

--- lib/helper_irtf.rb
+++ lib/helper_irtf.rb
@@ -341,10 +341,11 @@ module IRTF
   falk = Person.new("Aaron", "Falk", "aaron.falk@gmail.com", "http://linkedin.com/in/aaronfalk", nil)
   allman = Person.new("Mark", "Allman", "mallman@icir.org", "http://www.icir.org/mallman/", nil)
   dawkins = Person.new("Spencer", "Dawkins", "spencer@wonderhamster.org", "http://linkedin.com/in/spencerdawkins", nil)
-  arkko = Person.new("Jari", "Arkko", "jari.arkko@piuha.net", "http://www.arkko.com/", nil)
+  # arkko = Person.new("Jari", "Arkko", "jari.arkko@piuha.net", "http://www.arkko.com/", nil)
   li = Person.new("Xing", "Li", "xing@cernet.edu.cn", "http://www.net-glyph.org/lixing/", nil)
   lear = Person.new("Eliot", "Lear", "lear@cisco.com", "http://www.linkedin.com/pub/eliot-lear/0/81/b0a", nil)
-  $atlarge = [ falk, allman, dawkins, arkko, li, lear ]
+  housley = Person.new("Russ", "Housley", "housley@vigilsec.com", "http://www.linkedin.com/pub/russ-housley/0/69/419", nil)
+  $atlarge = [ falk, allman, dawkins, li, lear, housley ]
 
   def atlarge_contacts
     result = "irsg\n" +
