ce309ecdad2ec8ff48d53a0182b4c86efd24ee50
csp@csperkins.org <csp@csperkins.org>
2019-05-06 12:14:35 +0000

    Fix broken links to old IETF meetings

--- lib/filter_meeting.rb
+++ lib/filter_meeting.rb
@@ -11,7 +11,11 @@ class MeetingFilter < Nanoc::Filter
     elements = doc.xpath('//*[local-name() != "a" and not(ancestor::*[contains(concat(" ", normalize-space(@class), " "), " no-urlify ")]) and not(self::*[contains(concat(" ", normalize-space(@class), " "), " no-urlify ")])]/text()')
     elements.each do |element|
       element.content = element.content.gsub(/\bIETF-([0-9]{1,3}\b)/) {
-        |rfc| link_to("IETF-#{$1}", "https://ietf.org/how/meetings/#{$1}/")
+        |rfc| if $1.to_i < 91 then
+                link_to("IETF-#{$1}", "https://ietf.org/proceedings/#{$1}/index.html")
+              else
+                link_to("IETF-#{$1}", "https://ietf.org/how/meetings/#{$1}/")
+              end
       }
     end
     return doc.xpath('//body')[0].inner_html.gsub("&lt;", "<").gsub("&gt;", ">")
