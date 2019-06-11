fa6e0db17c209e31b3bc69668d9df69e968316b5
csp@csperkins.org <csp@csperkins.org>
2019-06-11 21:40:18 +0000

    Add ANRW 2019 accepted papers

--- lib/helper_anrw.rb
+++ lib/helper_anrw.rb
@@ -306,6 +306,98 @@ module ANRW
     return html
   end
 
+  def htmlify_paper_new3(file, nr)
+    p = case File.extname(file)
+    when ".json"
+      JSON.parse(File.read(file)).select{ |x| x["pid"] == nr }[0]
+    else
+      raise "Don't know how to handle #{file}"
+    end
+    #label = p["options"]["paper-talk"] == "Paper" ? "success" : "primary"
+    label = "success"
+    names = p["authors"].map { |a|
+      a["first"] + " " + a["last"] + (a.key?("affiliation") ?
+        " <em class=\"text-muted\">(" + a["affiliation"] + ")</em>" : "")
+    }
+
+    paper = p["paper"]
+    if p.has_key?("slides")
+      slides = File.join(File.dirname(file), p["slides"])
+    else 
+      slides = ""
+    end
+
+    html = %{
+      <div class="modal" id="modal#{nr}" tabindex="-1" role="dialog"
+           aria-labelledby="modallabel#{nr}">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal"
+                      aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+              <h4 class="modal-title" id="modallabel#{nr}">
+                <b>#{p["title"]}</b>
+              </h4>
+              #{names.to_sentence}
+            </div>
+            <div class="modal-body">
+              #{p["abstract"].gsub(/([[:alpha:]])-[[:space:]]+/, '\1')}
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default"
+                      data-dismiss="modal">
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-8">
+        <p>
+          <b>
+            #{p["title"] =~ /[[:punct:]]$/ ? p["title"] : p["title"] + "."}
+          </b>
+          <span class="label label-#{label}">Talk</span>
+          <br>
+          #{names.to_sentence + "."}
+        </p>
+      </div>
+      <div class="col-lg-4">
+        <p class="btn-toolbar">
+          <button class="btn btn-default btn-xs" type="button"
+                  data-toggle="modal" data-target="#modal#{nr}">
+            <span class="glyphicon glyphicon-align-justify" aria-hidden="true"></span>
+            Abstract
+          </button>
+    }
+
+    if File.exist?(slides)
+      html += %{
+          <a href="#{File.basename(slides)}"
+             class="btn btn-default btn-xs" role="button">
+            <span class="glyphicon glyphicon-picture" aria-hidden="true"></span>
+            Slides
+          </a>
+      }
+    end
+
+    if !paper.nil?
+      html += %{
+        <a href="#{paper}"
+           class="btn btn-default btn-xs" role="button">
+          <span class="glyphicon glyphicon-file" aria-hidden="true"></span>
+          Paper
+        </a>
+      }
+    end
+
+    html += "</p></div>"
+    return html
+  end
+
   def htmlify_poster(file, nr)
     p = case File.extname(file)
     when ".json"
